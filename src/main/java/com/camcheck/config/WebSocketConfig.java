5dc54b5ac0cfdd32b47d230bea5e9d537f0de21b
KaiTran <kaitran2205@gmail.com>
2025-07-21 14:46:11 +0700

    save

--- src/main/java/com/camcheck/config/WebSocketConfig.java
+++ src/main/java/com/camcheck/config/WebSocketConfig.java
@@ -5,8 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-import lombok.NonNull;
+import org.springframework.lang.NonNull;
 
 /**
  * WebSocket configuration for streaming camera footage
@@ -16,14 +15,13 @@ import lombok.NonNull;
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    @NonNull
-    public void configureMessageBroker( MessageBrokerRegistry config) {
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
